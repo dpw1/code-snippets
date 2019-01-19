b01d775856134ab201c07abcbcafa80cc111059f
dpw1 <diego.boarutto.fortes@gmail.com>
2019-01-19 20:22:44 +0000

    [RFC] Javascript Folder [ADD] Sum All

--- Javascript/UTIL - Rule of Three.js
+++ Javascript/UTIL - Rule of Three.js
@@ -11,14 +11,3 @@ If a 50 euros product has 1.30 euros of tax, how much tax does a 42 euros produc
 var ruleOfThree = ((a,b,c) =>  parseFloat((b*c)/a ));
 ruleOfThree(50, 1.3, 42); //1.092
 
-/*
-## Intro:
-Returns biggest int out of multiple values.
-
-## Example:
-
-## Usage:
-*/
-var biggestNumber = (...values) => Math.max(...values.map(e => parseInt(e)));
-biggestNumber(2,3,4,5,1,24,2,4,35,34,65,56,6,7,67,45,345,45,46,34,7798,89,89,67,55) //7798
-
---
+++ Javascript/UTIL - Sum All.js
@@ -0,0 +1,24 @@
+/*
+## Intro:
+Sum all.
+Sum all passed variables (string, array, int, float);
+
+## Usage:
+*/
+function sumAll(){
+	let items = [];
+	Array.from(arguments).map(item => {
+		if (Array.isArray(item)){
+			item.map(item2 => items.push(parseFloat(item2)))
+		} else{
+			items.push(parseFloat(item))
+		}
+	});
+	return items.reduce((prev, next) => prev + next, 0);
+}
+sumAll(1,2,3) //6
+sumAll('1', '2', '3'); //6
+sumAll(1,[1,2]); //4
+sumAll(1,'1',1); //3
+sumAll(1,'1',1, [1,2,3]); //9
+sumAll('1.1', '2.09', '3', [1.04,2.3], [1.8,2.2], 1); //14.530000000000001
