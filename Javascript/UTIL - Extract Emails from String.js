402cfecc6dcbee891c72734cf88b7130c9d22abd
dpw1 <diego.boarutto.fortes@gmail.com>
2019-02-22 05:54:53 +0000

    [FIX] email is not extracting images anymore

--- Javascript/UTIL - Extract Emails from String.js
+++ Javascript/UTIL - Extract Emails from String.js
@@ -8,7 +8,13 @@ Returns:
 ## Usage:
 */
 const extractEmails = (text) => {
-	const emails = text.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi);
+	let emails = [];
+	const emailsTemp = text.match(/(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/gi);
+	
+	emailsTemp.map(e => {
+		e.match(/(.jpg|.jpeg|.png|.gif|.mov|.mp4|.mp3|.avi|.wmv)$/gmi) ? null : emails.push(e);
+	});
+	
     return !!emails ? [...new Set(emails)] : false;
 }
 //extracts emails from a webpage
