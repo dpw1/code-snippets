93d44b379b20032d4de3e273a67e90400969c693
dpw1 <diego.boarutto.fortes@gmail.com>
2020-09-17 08:43:55 -0300

    [ADD] credits

--- Javascript/Basic Structure.js
+++ Javascript/Basic Structure.js
@@ -10,6 +10,10 @@ ezfy = (function() {
     const selectors = {
         example: '#example',
     };
+	
+	function credits(){
+  	var e=["background: linear-gradient(-47deg,#8731e8,#4528dc)","border: 1px solid #3E0E02","color: white","display: block","text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)","box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset","line-height: 40px","text-align: center","font-weight: bold","padding: 0px 5px"].join(";");function r(e){return(e+"").replace(/&#\d+;/gm,function(e){return String.fromCharCode(e.match(/\d+/gm)[0])})}var n=r(`&#67;&#117;&#115;&#116;&#111;&#109;&#32;&#99;&#111;&#100;&#101;&#100;&#32;&#98;&#121;&#32;&#104;&#116;&#116;&#112;&#115;&#58;&#47;&#47;&#101;&#122;&#102;&#121;&#99;&#111;&#100;&#101;&#46;&#99;&#111;&#109;`);console.log(`%c ${n}`,e);
+	}
 
     function _moveDOMElement(parent, child) {
         document.querySelector(parent).appendChild(document.querySelector(child));
