0976ad669fa77e11bf81dac8f1ab37b47a6d980e
dpw1 <diego.boarutto.fortes@gmail.com>
2020-12-14 05:22:55 -0300

    [UPD] wait until element exists

--- Javascript/Basic Structure.js
+++ Javascript/Basic Structure.js
@@ -7,10 +7,8 @@ Basic structure for a Shopify Javascript customization.
 window.ezfy = window.ezfy || {};
 
 ezfy = (function() {
-    
-
     function _loadScript(src) {
-        return new Promise(function (resolve, reject) {
+        return new Promise(function(resolve, reject) {
             var s;
             s = document.createElement('script');
             s.src = src;
@@ -21,9 +19,17 @@ ezfy = (function() {
     }
 
 
-	function credits(){
-  	var e=["background: linear-gradient(-47deg,#8731e8,#4528dc)","border: 1px solid #3E0E02","color: white","display: block","text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)","box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset","line-height: 40px","text-align: center","font-weight: bold","padding: 0px 5px"].join(";");function r(e){return(e+"").replace(/&#\d+;/gm,function(e){return String.fromCharCode(e.match(/\d+/gm)[0])})}var n=r(`&#67;&#117;&#115;&#116;&#111;&#109;&#32;&#99;&#111;&#100;&#101;&#100;&#32;&#98;&#121;&#32;&#104;&#116;&#116;&#112;&#115;&#58;&#47;&#47;&#101;&#122;&#102;&#121;&#99;&#111;&#100;&#101;&#46;&#99;&#111;&#109;`);console.log(`%c ${n}`,e);
-	}
+    function credits() {
+        var e = ["background: linear-gradient(-47deg,#8731e8,#4528dc)", "border: 1px solid #3E0E02", "color: white", "display: block", "text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)", "box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset", "line-height: 40px", "text-align: center", "font-weight: bold", "padding: 0px 5px"].join(";");
+
+        function r(e) {
+            return (e + "").replace(/&#\d+;/gm, function(e) {
+                return String.fromCharCode(e.match(/\d+/gm)[0])
+            })
+        }
+        var n = r(`&#67;&#117;&#115;&#116;&#111;&#109;&#32;&#99;&#111;&#100;&#101;&#100;&#32;&#98;&#121;&#32;&#104;&#116;&#116;&#112;&#115;&#58;&#47;&#47;&#101;&#122;&#102;&#121;&#99;&#111;&#100;&#101;&#46;&#99;&#111;&#109;`);
+        console.log(`%c ${n}`, e);
+    }
 
     function _moveDOMElement(parent, child) {
         document.querySelector(parent).appendChild(document.querySelector(child));
@@ -37,14 +43,33 @@ ezfy = (function() {
         return /cart/.test(window.location.href);
     }
 
-    function _waitUntilElementExists(selector, callback) {
-        const el = document.querySelector(selector);
+    function waitForElement(selector, delay = 1000, tries = 10) {
+        const element = document.querySelector(selector);
 
-        if (el) {
-            return callback(el);
+        // creates a local variable w/ the name of the selector to keep track of all tries
+        if (!window[`__${selector}`]) {
+            window[`__${selector}`] = 0;
         }
 
-        setTimeout(() => _waitUntilElementExists(selector, callback), 500);
+        function _search() {
+            return new Promise((resolve) => {
+                window[`__${selector}`]++;
+                console.log(window[`__${selector}`]);
+                setTimeout(resolve, delay);
+            });
+        }
+
+        //element not found, retry
+        if (element === null) {
+            if (window[`__${selector}`] >= tries) {
+                window[`__${selector}`] = 0;
+                return Promise.reject(null);
+            }
+
+            return _search().then(() => waitForElement(selector));
+        } else {
+            return Promise.resolve(element);
+        }
     }
 
     function _addStyle(styleString) {
@@ -54,27 +79,27 @@ ezfy = (function() {
     }
 
     function _observeDOM() {
-		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-			eventListenerSupported = window.addEventListener;
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+            eventListenerSupported = window.addEventListener;
 
-		return function (obj, callback) {
-			if (MutationObserver) {
-				// define a new observer
-				var obs = new MutationObserver(function (mutations, observer) {
-					if (mutations[0].addedNodes.length || mutations[0].removedNodes.length)
-						callback();
-				});
+        return function(obj, callback) {
+            if (MutationObserver) {
+                // define a new observer
+                var obs = new MutationObserver(function(mutations, observer) {
+                    if (mutations[0].addedNodes.length || mutations[0].removedNodes.length)
+                        callback();
+                });
 
-				obs.observe(obj, {
-					childList: true,
-					subtree: true
-				});
-			} else if (eventListenerSupported) {
-				obj.addEventListener('DOMNodeInserted', callback, false);
-				obj.addEventListener('DOMNodeRemoved', callback, false);
-			}
-		};
-	}
+                obs.observe(obj, {
+                    childList: true,
+                    subtree: true
+                });
+            } else if (eventListenerSupported) {
+                obj.addEventListener('DOMNodeInserted', callback, false);
+                obj.addEventListener('DOMNodeRemoved', callback, false);
+            }
+        };
+    }
 
     function hello() {
         console.log('EZFY is on')
