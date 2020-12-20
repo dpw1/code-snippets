4d7469ea041913221f062273a585350df1a3e22c
dpw1 <diego.boarutto.fortes@gmail.com>
2020-12-19 22:56:30 -0300

    [UPD] load styles function

--- Javascript/Basic Structure.js
+++ Javascript/Basic Structure.js
@@ -17,6 +17,19 @@ ezfy = (function() {
             document.head.appendChild(s);
         });
     }
+	
+	function _loadStyle(src) {
+        return new Promise(function(resolve, reject) {
+            let link = document.createElement('link');
+            link.href = src;
+            link.rel = 'stylesheet';
+
+            link.onload = () => resolve(link);
+            link.onerror = () => reject(new Error(`Style load error for ${src}`));
+
+            document.head.append(link);
+        });
+    }
 
 
     function credits() {
